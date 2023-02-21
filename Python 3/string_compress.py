41e1246e7216bacdf9b1dc78a839e2b3bc3abee6
NFA <novran@zerobyte.id>
2023-02-21 14:59:09 +0700

    Update string_compress.py

--- Python 3/string_compress.py
+++ Python 3/string_compress.py
@@ -8,3 +8,7 @@ def strdecompress(string):
 def strcompress(string):
 	compressed_data = gzip.compress(bytes(string, 'utf-8'))
 	return compressed_data
+
+jsonstr = str(json.dumps({'name': 'test'}))
+comdata = strcompress(jsonstr)
+decdata = json.loads(strdecompress(comdata))
